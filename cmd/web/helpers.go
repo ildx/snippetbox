e28f5fef039ca46d25cc4a21d93ac40fd47bccf0
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-17 10:41:34 +0200

    caching templates

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,21 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
-  var (
-    method = r.Method
-    uri = r.URL.RequestURI()
-  )
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
 
-  app.logger.Error(err.Error(), "method", method, "uri", uri)
-  http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
-  http.Error(w, http.StatusText(status), status)
+	http.Error(w, http.StatusText(status), status)
 }
 
 func (app *application) notFound(w http.ResponseWriter) {
-  app.clientError(w, http.StatusNotFound)
+	app.clientError(w, http.StatusNotFound)
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(status)
+
+	err := ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,40 +1,49 @@
 package main
 
 import (
-  "database/sql"
+	"database/sql"
 	"flag"
+	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
 
-  "github.com/ildx/snippetbox/internal/models"
+	"github.com/ildx/snippetbox/internal/models"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
-  snippets *models.SnippetModel
+	logger        *slog.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-  dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-  db, err := openDB(*dsn)
-  if err != nil {
-    logger.Error(err.Error())
-    os.Exit(1)
-  }
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 
-  defer db.Close()
+	defer db.Close()
+
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 
 	app := &application{
-		logger: logger,
-    snippets: &models.SnippetModel{DB: db},
+		logger:        logger,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	logger.Info("starting server", "addr", *addr)
@@ -45,16 +54,16 @@ func main() {
 }
 
 func openDB(dsn string) (*sql.DB, error) {
-  db, err := sql.Open("mysql", dsn)
-  if err != nil {
-    return nil, err
-  }
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
 
-  err = db.Ping()
-  if err != nil {
-    db.Close()
-    return nil, err
-  }
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
 
-  return db, nil
+	return db, nil
 }
