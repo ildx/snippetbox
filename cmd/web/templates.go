f7385406722e4bc2bc1dc70f9b2f5cb5d31f44e9
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-17 11:11:40 +0200

    custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,46 +1,55 @@
 package main
 
 import (
-  "html/template"
-  "path/filepath"
+	"html/template"
+	"path/filepath"
+	"time"
 
-  "github.com/ildx/snippetbox/internal/models"
+	"github.com/ildx/snippetbox/internal/models"
 )
 
 type templateData struct {
-  CurrentYear int
-  Snippet models.Snippet
-  Snippets []models.Snippet
+	CurrentYear int
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
-  cache := map[string]*template.Template{}
+	cache := map[string]*template.Template{}
 
-  pages, err := filepath.Glob("./ui/html/pages/*.html")
-  if err != nil {
-    return nil, err
-  }
+	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	if err != nil {
+		return nil, err
+	}
 
-  for _, page := range pages {
-    name := filepath.Base(page)
+	for _, page := range pages {
+		name := filepath.Base(page)
 
-    ts, err := template.ParseFiles("./ui/html/base.html")
-    if err != nil {
-      return nil, err
-    }
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
+		if err != nil {
+			return nil, err
+		}
 
-    ts, err = ts.ParseGlob("./ui/html/partials/*.html")
-    if err != nil {
-      return nil, err
-    }
+		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
+		if err != nil {
+			return nil, err
+		}
 
-    ts, err = ts.ParseFiles(page)
-    if err != nil {
-      return nil, err
-    }
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
 
-    cache[name] = ts
-  }
+		cache[name] = ts
+	}
 
-  return cache, nil
+	return cache, nil
 }
