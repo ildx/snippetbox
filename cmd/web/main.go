19e4db7ffea7d60045762a14edc290eeda804c47
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-20 17:54:58 +0200

    create a user model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "crypto/tls"
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -21,6 +21,7 @@ import (
 type application struct {
 	logger         *slog.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
@@ -52,28 +53,29 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
-  sessionManager.Cookie.Secure = true
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		logger:         logger,
 		snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
 		templateCache:  templateCache,
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
 	}
 
-  tlsConfig := &tls.Config{
-    CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
-  }
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
 
 	srv := &http.Server{
-		Addr:     *addr,
-		Handler:  app.routes(),
-		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
-    TLSConfig: tlsConfig,
-    IdleTimeout: time.Minute,
-    ReadTimeout: 5 * time.Second,
-    WriteTimeout: 10 * time.Second,
+		Addr:         *addr,
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
